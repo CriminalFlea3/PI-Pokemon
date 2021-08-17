595cf5ec6c40255692878e4d87027fa6ea28889a
Kevin Aguilera <kevin.lovo@gmail.com>
2021-08-16 19:52:39 -0500

    Cambios en la ruta types para evitar error de sobreescritura

--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -8,6 +8,8 @@ router.get('/', async (req, res) => {
     const api = await fetch('https://pokeapi.co/api/v2/type');
     const types = await api.json();
     for( t of types.results ) {
+        const existe = await Tipo.findOne({where: { name: t.name }})
+        if(existe) return res.send('Ya existe')
         await Tipo.create({ name: t.name})
     }
     const type = await Tipo.findAll();
